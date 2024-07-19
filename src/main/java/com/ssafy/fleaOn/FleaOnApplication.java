44d1eeb8414c8345fbe030cb1518e81d19d0b48d
김용원 <qwsa522@naver.com>
2024-07-19 11:05:57 +0900

    initial commit

---
+++ src/main/java/com/ssafy/fleaOn/FleaOnApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.fleaOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleaOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/fleaOn/FleaOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fleaOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleaOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
