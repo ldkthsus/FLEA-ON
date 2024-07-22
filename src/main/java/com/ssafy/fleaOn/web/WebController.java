c5a781f64f29e6ab403f87c9b2044e4368c53b42
김용원 <qwsa522@naver.com>
2024-07-22 16:03:57 +0900

    build : solve integ build

---
+++ src/main/java/com/ssafy/fleaOn/web/WebController.java
@@ -0,0 +1,13 @@
+package com.ssafy.fleaOn.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("*")
+    public String forward() {
+        return "index.html";
+    }
+}
