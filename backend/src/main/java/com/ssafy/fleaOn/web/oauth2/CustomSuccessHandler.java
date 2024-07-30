a18c61015ff60d54fd384a327ce3fb2cb9e447ae
김용원 <qwsa522@naver.com>
2024-07-30 14:29:27 +0900

    fix : 리다이렉트 경로 변경4

--- backend/src/main/java/com/ssafy/fleaOn/web/oauth2/CustomSuccessHandler.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/oauth2/CustomSuccessHandler.java
@@ -41,7 +41,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String token = jwtUtil.createJwt(username, role, 60*60*60*60L);
 
         response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect("http://localhost:3000");
+        response.sendRedirect("http://localhost:3000/check?access_token=" + token);
+
     }
 
     private Cookie createCookie(String key, String value) {
