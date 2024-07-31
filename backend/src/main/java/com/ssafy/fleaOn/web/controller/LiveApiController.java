c39fe164f150e282aa943990a41b03fb08d21377
라송연 <skthddus123@naver.com>
2024-07-31 13:27:19 +0900

    Save changes before switching branches

--- backend/src/main/java/com/ssafy/fleaOn/web/controller/LiveApiController.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.fleaOn.web.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequiredArgsConstructor
-@RestController
-public class LiveApiController {
-}
