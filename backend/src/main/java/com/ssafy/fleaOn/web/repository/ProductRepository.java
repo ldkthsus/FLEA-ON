74c1b3f9b014a37267481524a3148632ddfd55ad
라송연 <skthddus123@naver.com>
2024-07-31 15:07:10 +0900

    fix : backend branch 코드랑 merge

--- backend/src/main/java/com/ssafy/fleaOn/web/repository/ProductRepository.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.ssafy.fleaOn.web.repository;
 import com.ssafy.fleaOn.web.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    Product findById(int id);
+    Optional<Product> findById(int id);
 }
