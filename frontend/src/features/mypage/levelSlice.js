89db49e6c231fccd1acd1168800546d6e79f6de1
gimhaen <snnsprng043@gmail.com>
2024-08-01 02:46:45 +0900

    fix: 판매개수에 따른 레벨 컴포넌트 구현 근데 무슨 캐시 문제인지 앱이 무한 새로고침 된다... 그리고 왜인지 이미지 파일이 다 깨졌다 아까는 잘 됐는데...

--- frontend/src/features/mypage/levelSlice.js
+++ frontend/src/features/mypage/levelSlice.js
@@ -1,26 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const levels = [
+  {
+    name: "아기손",
+    minSales: 0,
+    maxSales: 1,
+    icon: "../../assets/images/level_baby.svg",
+  },
+  {
+    name: "작은손",
+    minSales: 1,
+    maxSales: 6,
+    icon: "../../assets/images/level_small.svg",
+  },
+  {
+    name: "중간손",
+    minSales: 6,
+    maxSales: 26,
+    icon: "../../assets/images/level_middle.svg",
+  },
+  {
+    name: "큰손",
+    minSales: 26,
+    maxSales: "MAX",
+    icon: "../../assets/images/level_big.svg",
+  },
+];
+
 const levelSlice = createSlice({
   name: "level",
   initialState: {
     salesCount: 0,
-    level: "기본손",
+    level: levels[0].name,
+    nextLevel: levels[1].name,
+    salesGoal: levels[0].maxSales - levels[0].minSales,
+    currentLevelSales: 0,
   },
   reducers: {
     updateSalesCount: (state, action) => {
       const salesCount = action.payload;
-      let level;
-      if (salesCount === 0) {
-        level = "기본손";
-      } else if (salesCount >= 1 && salesCount <= 3) {
-        level = "아기손";
-      } else if (salesCount > 3 && salesCount <= 20) {
-        level = "중간손";
-      } else {
-        level = "큰손";
+      let currentLevel, nextLevel;
+
+      for (let i = 0; i < levels.length; i++) {
+        if (salesCount < levels[i].maxSales || levels[i].maxSales === "MAX") {
+          currentLevel = levels[i];
+          nextLevel = levels[i + 1] || null;
+          break;
+        }
       }
+
       state.salesCount = salesCount;
-      state.level = level;
+      state.level = currentLevel.name;
+      state.nextLevel = nextLevel ? nextLevel.name : null;
+      state.levelIcon = currentLevel.icon;
+      state.nextLevelIcon = nextLevel ? nextLevel.icon : null;
+      state.salesGoal =
+        currentLevel.maxSales === "MAX"
+          ? "MAX"
+          : currentLevel.maxSales - currentLevel.minSales;
+      state.currentLevelSales = salesCount - currentLevel.minSales;
     },
   },
 });
