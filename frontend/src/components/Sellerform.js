3714591d75b8ad476d219f2f3aa88d0a1e24891d
9oogone <attractiveness@naver.com>
2024-08-01 17:15:30 +0900

    design: slotProps 사용하여 MUI 커스텀, 달력 너비를 조정하여 시간 선택이 잘리지 않도록 함

--- frontend/src/components/Sellerform.js
+++ frontend/src/components/Sellerform.js
@@ -1,28 +1,22 @@
-import React, { useState, useCallback } from 'react';
-import PortalPopup from "./PortalPopup";
+import React, { useState } from 'react';
 import styles from '../styles/SellerForm.module.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker, DesktopDateTimePicker } from '@mui/x-date-pickers';
+import TextField from '@mui/material/TextField';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+
+// 로케일을 한국어로 설정
+dayjs.locale('ko');
 
 const SellerformSelect = () => {
-  const [isDateSelectOpen, setDateSelectOpen] = useState(false);
-  const [isTimeSelectOpen, setTimeSelectOpen] = useState(false);
   const [thumbnail, setThumbnail] = useState(null);
   const [thumbnailPreview, setThumbnailPreview] = useState(null);
 
-  const openDateSelect = useCallback(() => {
-    setDateSelectOpen(true);
-  }, []);
-
-  const closeDateSelect = useCallback(() => {
-    setDateSelectOpen(false);
-  }, []);
-
-  const openTimeSelect = useCallback(() => {
-    setTimeSelectOpen(true);
-  }, []);
-
-  const closeTimeSelect = useCallback(() => {
-    setTimeSelectOpen(false);
-  }, []);
+  const [startDate, setStartDate] = useState(dayjs());
+  const [endDate, setEndDate] = useState(dayjs());
 
   const handleThumbnailChange = (event) => {
     const file = event.target.files[0];
@@ -61,135 +55,77 @@ const SellerformSelect = () => {
                 <input type="text" className={styles.inputField} placeholder="라이브 방송 제목" />
               </div>
             </div>
-            <div className={styles.dateAndTimeCompactColl}>
-              <div className={styles.date}>
-                <div className={styles.year}>7월 21일</div>
-              </div>
-              <div className={styles.time}>
-                <div className={styles.year}>오후 08:00</div>
-              </div>
+            <div className='livestarttime'>
+              <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+                <DemoContainer components={['DesktopDateTimePicker']}>
+                  <DesktopDateTimePicker
+                    label="라이브 방송 시간"
+                    value={startDate}
+                    onChange={(newValue) => setStartDate(newValue)}
+                    renderInput={(params) => <TextField {...params} />}
+                    ampm={true}  // 24시간 형식
+                    disablePast={true}  // 과거 날짜 선택 비활성화
+                    slotProps={{ // 달력 사이즈 조절하여 시간 선택 부분이 잘리지 않도록 조정
+                      popper: {
+                        sx: {
+                          '& .MuiDateCalendar-root': {
+                            overflow: 'hidden',
+                            width: '250px',
+                            maxHeight: '336px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            margin: '0 auto',
+                            height: '336px',
+                          },
+                        },
+                      },
+                    }}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
             </div>
-          </div>
-          <div className={styles.thumbnailContainer}>
-            <input 
-              type="file" 
-              accept="image/*" 
-              onChange={handleThumbnailChange} 
-              className={styles.thumbnailInput}
-            />
-            {thumbnailPreview && (
-              <img 
-                className={styles.thumbnailPreview} 
-                src={thumbnailPreview} 
-                alt="Thumbnail Preview" 
+            <div className={styles.thumbnailContainer}>
+              <input 
+                type="file" 
+                accept="image/*" 
+                onChange={handleThumbnailChange} 
+                className={styles.thumbnailInput}
               />
-            )}
-          </div>
-          <div className={styles.closeButton}>
-            <img className={styles.phxBoldIcon} alt="" src="ph:x-bold.svg" />
+              {thumbnailPreview && (
+                <img 
+                  className={styles.thumbnailPreview} 
+                  src={thumbnailPreview} 
+                  alt="Thumbnail Preview" 
+                />
+              )}
+            </div>
           </div>
         </div>
         <div className={styles.sellerTime}>
           <div className={styles.div1}>거래 가능 시간</div>
-          <div className={styles.dateParent}>
-            <div className={styles.date1}>
-              <div className={styles.month} />
-              <div className={styles.year}>7월 21일</div>
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+            <div className={styles.dateParent}>
+              <DatePicker
+                label="시작 날짜"
+                value={startDate}
+                onChange={(newValue) => setStartDate(newValue)}
+                renderInput={(params) => <TextField {...params} />}
+                inputFormat="YYYY년 MM월 DD일"
+              />
+              <DatePicker
+                label="종료 날짜"
+                value={endDate}
+                onChange={(newValue) => setEndDate(newValue)}
+                renderInput={(params) => <TextField {...params} />}
+                inputFormat="YYYY년 MM월 DD일"
+              />
             </div>
-            <div className={styles.time2}>
-              <div className={styles.year}>오후 08:30</div>
-            </div>
-            <div className={styles.div2}>~</div>
-            <div className={styles.time2}>
-              <div className={styles.year}>오후 10:00</div>
-            </div>
-            <img className={styles.vectorIcon} alt="" src="Vector.svg" />
-          </div>
-          <div className={styles.dateGroup}>
-            <div className={styles.date2} onClick={openDateSelect}>
-              <div className={styles.month} />
-              <div className={styles.year}>날짜</div>
-              <img className={styles.arrowDropDownIcon} alt="" src="arrow_drop_down.svg" />
-            </div>
-            <div className={styles.div3}>부터</div>
-            <div className={styles.div4}>까지</div>
-            <div className={styles.date3} onClick={openTimeSelect}>
-              <div className={styles.month} />
-              <div className={styles.year3}>시간</div>
-              <img className={styles.arrowDropDownIcon} alt="" src="arrow_drop_down.svg" />
-            </div>
-            <div className={styles.date4} onClick={openTimeSelect}>
-              <div className={styles.month} />
-              <div className={styles.year3}>시간</div>
-              <img className={styles.arrowDropDownIcon} alt="" src="arrow_drop_down.svg" />
-            </div>
-          </div>
-          <div className={styles.parent}>
-            <div className={styles.div5}>시간 추가</div>
-            <img className={styles.addIcon} alt="" src="add.svg" />
-          </div>
-        </div>
-        <div className={styles.sellerAddress}>
-          <div className={styles.div6}>거래 가능 장소</div>
-          <div className={styles.dateAndTimeCompactColl1}>
-            <div className={styles.date5}>
-              <div className={styles.month4}>2024</div>
-              <div className={styles.year5}>대전광역시 유성구 동서대로 98-39</div>
-            </div>
-            <div className={styles.date6}>
-              <div className={styles.month4}>2024</div>
-              <div className={styles.year}>삼성화재 유성연수원</div>
-            </div>
-            <div className={styles.time6}>
-              <div className={styles.year}>오후 08:00</div>
-            </div>
-          </div>
-          <div className={styles.dateAndTimeCompactColl2}>
-            <div className={styles.date6}>
-              <div className={styles.month4}>2024</div>
-              <div className={styles.year}>경비실 앞</div>
-            </div>
-            <div className={styles.time6}>
-              <div className={styles.year}>오후 08:00</div>
-            </div>
-          </div>
-          <div className={styles.div7}>상세주소</div>
-        </div>
-        <div className={styles.sellerItem}>
-          <div className={styles.sellerItemChild} />
-          <div className={styles.wrapper}>
-            <div className={styles.div8}>판매목록</div>
-          </div>
-          <div className={styles.item}>
-            <div className={styles.div9}>자전거 따릉이</div>
-            <div className={styles.div10}>5만 5000원</div>
-          </div>
+          </LocalizationProvider>
         </div>
       </div>
-      <img className={styles.addIcon1} alt="" src="add.svg" />
       <div className={styles.button}>
         <button type="submit" className={styles.button1}>등록하기</button>
       </div>
-      <div className={styles.sellerformSelectChild} />
-      <img className={styles.addIcon2} alt="" src="add.svg" />
-      {isDateSelectOpen && (
-        <PortalPopup
-          overlayColor="rgba(113, 113, 113, 0.3)"
-          placement="Centered"
-          onOutsideClick={closeDateSelect}
-        >
-          {/* <DateSelect onClose={closeDateSelect} /> */}
-        </PortalPopup>
-      )}
-      {isTimeSelectOpen && (
-        <PortalPopup
-          overlayColor="rgba(113, 113, 113, 0.3)"
-          placement="Centered"
-          onOutsideClick={closeTimeSelect}
-        >
-          {/* <TimeSelect onClose={closeTimeSelect} /> */}
-        </PortalPopup>
-      )}
     </form>
   );
 };
