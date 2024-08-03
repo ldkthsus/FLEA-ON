317ad498ccd4af82673277fa9b446aee2def0fb3
minji <minipeach0923@gmail.com>
2024-08-03 16:39:18 +0900

    feat : redis 설정 중

---
+++ backend/src/main/java/com/ssafy/fleaOn/web/service/RedisService.java
@@ -0,0 +1,20 @@
+package com.ssafy.fleaOn.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+}
