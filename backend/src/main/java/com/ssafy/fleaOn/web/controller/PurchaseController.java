d166328e856c0a8ebd5edaf5e64921b06cf418de
minji <minipeach0923@gmail.com>
2024-08-04 01:03:50 +0900

    fix : 구매 로직 수정 완료, 구매확정 추가 수정 필요

--- backend/src/main/java/com/ssafy/fleaOn/web/controller/PurchaseController.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/controller/PurchaseController.java
@@ -1,8 +1,10 @@
 package com.ssafy.fleaOn.web.controller;
 
 import com.ssafy.fleaOn.web.dto.PurchaseRequest;
+import com.ssafy.fleaOn.web.dto.TradeRequest;
 import com.ssafy.fleaOn.web.producer.RedisQueueProducer;
 import com.ssafy.fleaOn.web.service.PurchaseService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,32 +23,37 @@ public class PurchaseController {
     }
 
     @PostMapping("/buy")
+    @Operation(summary = "구매(줄서기) 버튼 클릭", description = "구매의사를 가지고 버튼을 클릭합니다.")
     public ResponseEntity<Integer> buy(@RequestBody PurchaseRequest request) {
         int order = purchaseService.processPurchaseRequest(request);
-        return ResponseEntity.ok(order);
+        return ResponseEntity.ok(order); // 구매 순번 반환
     }
 
-    @PostMapping("/cancel")
+    @DeleteMapping("/cancel")
+    @Operation(summary = "구매 취소 기능", description = "구매 취소를 합니다.")
     public ResponseEntity<Integer> cancel(@RequestBody PurchaseRequest request) {
         int buyerId = purchaseService.processCancelPurchaseRequest(request);
-        return ResponseEntity.ok(buyerId);
+        return ResponseEntity.ok(buyerId); // 취소 후 현재 구매자 반환
     }
 
-    @PostMapping("/reserve")
-    public ResponseEntity<Void> reserve(@RequestBody PurchaseRequest request) {
-        purchaseService.processReservationRequest(request);
-        return ResponseEntity.ok().build();
+    @PutMapping("/reserve")
+    @Operation(summary = "예약하기", description = "예약버튼이 활성화되어있을 시 예약합니다.")
+    public ResponseEntity<Integer> reserve(@RequestBody PurchaseRequest request) {
+        int order = purchaseService.processReservationRequest(request);
+        return ResponseEntity.ok(order); // 구매 순번 반환
     }
 
-    @PostMapping("/cancel-reservation")
-    public ResponseEntity<Void> cancelReservation(@RequestBody PurchaseRequest request) {
-        purchaseService.processCancelReservationRequest(request);
-        return ResponseEntity.ok().build();
+    @DeleteMapping("/reserve/")
+    @Operation(summary = "예약 취소하기", description = "예약을 취소합니다.")
+    public ResponseEntity<Integer> cancelReservation(@RequestBody PurchaseRequest request) {
+        int count = purchaseService.processCancelReservationRequest(request);
+        return ResponseEntity.ok(count); // 현재 예약자 수 응답
     }
 
-//    @PostMapping("/confirm")
-//    public ResponseEntity<Void> confirm(@RequestBody PurchaseRequest request) {
-//        purchaseService.processConfirmPurchaseRequest(request);
-//        return ResponseEntity.ok().build();
-//    }
+    @PostMapping("/confirm")
+    @Operation(summary = "구매 확정하기", description = "구매 예정자가 구매 시간 설정 후 구매를 확정합니다.")
+    public ResponseEntity<Void> confirm(@RequestBody TradeRequest request) {
+        purchaseService.processConfirmPurchaseRequest(request);
+        return ResponseEntity.ok().build(); // 구매 확정 완료 응답
+    }
 }
