fd3916e8bf1d7a0b9cffe43f5df90a1173993cab
minji <minipeach0923@gmail.com>
2024-08-05 10:37:35 +0900

    fix : 라이브 정보 변경, 조회 데이터 추가

--- backend/src/main/java/com/ssafy/fleaOn/web/dto/LiveDetailResponse.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/dto/LiveDetailResponse.java
@@ -1,9 +1,13 @@
 package com.ssafy.fleaOn.web.dto;
 
 import com.ssafy.fleaOn.web.domain.Live;
+import com.ssafy.fleaOn.web.domain.LiveTradeTime;
 import com.ssafy.fleaOn.web.domain.Product;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,8 +20,10 @@ public class LiveDetailResponse {
     private final String tradePlace;
     private final boolean isLive;
     private final List<ProductResponse> products;
+    private final List<LiveTradeResponse> liveTradeTimes;
 
-    public LiveDetailResponse(Live live, List<Product> products) {
+
+    public LiveDetailResponse(Live live, List<Product> products, List<LiveTradeTime> liveTradeTimes) {
         this.liveId = live.getLiveId();
         this.title = live.getTitle();
         this.liveDate = live.getLiveDate().toString();
@@ -25,6 +31,7 @@ public class LiveDetailResponse {
         this.tradePlace = live.getTradePlace();
         this.isLive = live.getIsLive();
         this.products = products.stream().map(ProductResponse::new).collect(Collectors.toList());
+        this.liveTradeTimes = liveTradeTimes.stream().map(LiveTradeResponse::new).collect(Collectors.toList());
     }
 
     @Getter
@@ -41,6 +48,21 @@ public class LiveDetailResponse {
             this.price = product.getPrice();
             this.firstCategoryId = product.getFirstCategoryId();
             this.secondCategoryId = product.getSecondCategoryId();
+        }
+    }
+
+    @Getter
+    public static class LiveTradeResponse {
+        private final int timeId;
+        private final LocalTime tradeStart;
+        private final LocalTime tradeEnd;
+        private final LocalDate date;
+
+        public LiveTradeResponse (LiveTradeTime liveTradeTime){
+            this.timeId = liveTradeTime.getTimeId();
+            this.tradeStart = liveTradeTime.getTradeStart();
+            this.tradeEnd = liveTradeTime.getTradeEnd();
+            this.date = liveTradeTime.getDate();
         }
     }
 }
