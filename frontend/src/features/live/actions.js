2e8a9e55b5ee8e80e4677380e4e04681696e1f2f
cadetbluee <cadetbluee@icloud.com>
2024-08-06 16:01:14 +0900

    fix: 드디어 라이브 뒤로가면 라이브 종료됩니다

--- frontend/src/features/live/actions.js
+++ frontend/src/features/live/actions.js
@@ -2,13 +2,13 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// 상품 카테고리 조회 액션 -> 나오면 풀기
+// 상품 카테고리 조회 액션
 export const fetchCategories = createAsyncThunk('live/fetchCategories', async (productName) => {
   const response = await axios.get(`/api/categories?productName=${productName}`);
   return response.data;
 });
 
-// 라이브 방송 생성 액션 -> 백에게 물어보기
+// 라이브 방송 생성 액션
 export const createLiveBroadcast = createAsyncThunk(
     'live/createLiveBroadcast',
     async (formData, { getState }) => {
