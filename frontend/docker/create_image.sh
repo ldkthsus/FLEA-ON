f3b662dbe0e0f4e417567aecf1efcaa64bcd1659
김용원 <qwsa522@naver.com>
2024-08-06 09:39:09 +0900

    fix : 수정 한번만 할게요...

--- frontend/docker/create_image.sh
+++ frontend/docker/create_image.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "No version argument provided. Usage: \"./create_image.sh <IMAGE_NAME>\""
-    exit 1
-fi
-
 pushd ../
 docker build -f docker/Dockerfile -t "qsc753969/fleaonFront:latest" .
