efeef318a257aed6bd0525be8e35746d9a18b47d
snnsprng <snnsprng@naver.com>
2024-08-08 02:49:37 +0900

    feat: 채팅 거래상세 구현 및 css 거래 상세 리스트 구현 및 상태에 따라서 버튼 변경 스크롤 두개 구현

--- frontend/src/components/ChatTradeDetail.js
+++ frontend/src/components/ChatTradeDetail.js
@@ -17,11 +17,200 @@ const ChatTradeDetail = ({ isOpen, onClose }) => {
       statusColor: "#FF0B55",
     },
     {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
+      id: 1,
+      name: "마리아상",
+      price: "6만원",
+      status: "상품취소",
+      statusColor: "#FF0B55",
+    },
+    {
       id: 2,
       name: "마리아상",
       price: "6만원",
       status: "상품추가",
       statusColor: "#FF0B55",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
+    },
+    {
+      id: 3,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 서기",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 4,
+      name: "마리아상",
+      price: "6만원",
+      status: "줄 취소",
+      statusColor: "#FF5757",
+    },
+    {
+      id: 5,
+      name: "마리아상",
+      price: "6만원",
+      status: "출 마감",
+      statusColor: "#CCCCCC",
     },
     {
       id: 3,
@@ -165,14 +354,13 @@ const ChatTradeDetail = ({ isOpen, onClose }) => {
     <Box
       sx={{
         width: "100%",
-        height: "90%",
+        maxHeight: "90vh",
         position: "fixed",
         bottom: 0,
         left: "50%",
         transform: "translateX(-50%)",
         bgcolor: "background.paper",
         boxShadow: 24,
-        overflowY: "auto",
         display: "flex",
         flexDirection: "column",
         borderTopLeftRadius: 16,
@@ -180,55 +368,74 @@ const ChatTradeDetail = ({ isOpen, onClose }) => {
         zIndex: "1300",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          p: 3,
-          position: "relative",
-          borderBottom: "1px solid rgba(0, 0, 0, 0.20)",
-        }}
-      >
+      <Box sx={{ position: "sticky", top: 0, backgroundColor: "#ffffff" }}>
         <Box
-          onClick={handleBackButtonClick}
           sx={{
-            cursor: "pointer",
-            position: "absolute",
-            left: 20,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            p: 3,
+            position: "relative",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.20)",
           }}
         >
-          <ArrowBackIosNew />
-        </Box>
-        <Typography
-          sx={{
-            fontSize: "24px",
-            fontWeight: "600",
-            textAlign: "center",
-          }}
-        >
-          거래 상세
-        </Typography>
-      </Box>
-      <Box sx={{ p: 2, display: "flex", flexDirection: "column", gap: 2 }}>
-        <Box sx={{ display: "flex", gap: 1 }}>
-          <AccessTime sx={{ fontSize: 24 }} />
-          <Typography sx={{ fontSize: 18 }}>
-            2024. 07.23. 목요일 오후 3시
+          <Box
+            onClick={handleBackButtonClick}
+            sx={{
+              cursor: "pointer",
+              position: "absolute",
+              left: 20,
+            }}
+          >
+            <ArrowBackIosNew />
+          </Box>
+          <Typography
+            sx={{
+              fontSize: "24px",
+              fontWeight: "600",
+              textAlign: "center",
+            }}
+          >
+            거래 상세
           </Typography>
         </Box>
-        <Box sx={{ display: "flex", gap: 1 }}>
-          <PlaceOutlined sx={{ fontSize: 24 }} />
-          <Box>
+        <Box sx={{ p: 2, display: "flex", flexDirection: "column", gap: 2 }}>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <AccessTime sx={{ fontSize: 24 }} />
             <Typography sx={{ fontSize: 18 }}>
-              대전광역시 유성구 덕명동
+              2024년 7월 23일 목요일 오후 3시
             </Typography>
-            <Typography sx={{ fontSize: 18 }}>한밭대학교 주차장1 </Typography>
+          </Box>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <PlaceOutlined sx={{ fontSize: 24 }} />
+            <Box>
+              <Typography sx={{ fontSize: 18 }}>
+                대전광역시 유성구 덕명동
+              </Typography>
+              <Typography sx={{ fontSize: 18 }}>
+                한밭대학교 주차장1 에서
+              </Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 5, p: 2 }}>
-        <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          flexGrow: 1,
+          px: 1,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 0.5,
+            flexGrow: 1,
+          }}
+        >
           <Box
             sx={{
               borderBottom: "1px solid rgba(0, 0, 0, 0.20)",
@@ -238,16 +445,25 @@ const ChatTradeDetail = ({ isOpen, onClose }) => {
           >
             <Typography sx={{ fontWeight: "800" }}>거래 예정 상품</Typography>
           </Box>
-          {scheduledProducts.map((product) => (
-            <ProductItem
-              key={product.id}
-              product={product}
-              onStatusClick={handleStatusClick}
-            />
-          ))}
+          <Box
+            sx={{
+              maxHeight: `calc((90vh - 320px) / 2)`,
+              overflowY: "auto",
+            }}
+          >
+            {scheduledProducts.map((product) => (
+              <ProductItem
+                key={product.id}
+                product={product}
+                onStatusClick={handleStatusClick}
+              />
+            ))}
+          </Box>
         </Box>
 
-        <Box>
+        <Box
+          sx={{ display: "flex", flexDirection: "column", gap: 2, flexGrow: 1 }}
+        >
           <Box
             sx={{
               borderBottom: "1px solid rgba(0, 0, 0, 0.20)",
@@ -259,13 +475,20 @@ const ChatTradeDetail = ({ isOpen, onClose }) => {
               '라이브방제'의 다른 상품(거래완료 전인것만)
             </Typography>
           </Box>
-          {otherProducts.map((product) => (
-            <ProductItem
-              key={product.id}
-              product={product}
-              onStatusClick={handleStatusClick}
-            />
-          ))}
+          <Box
+            sx={{
+              maxHeight: `calc((90vh - 300px) / 2)`,
+              overflowY: "auto",
+            }}
+          >
+            {otherProducts.map((product) => (
+              <ProductItem
+                key={product.id}
+                product={product}
+                onStatusClick={handleStatusClick}
+              />
+            ))}
+          </Box>
         </Box>
       </Box>
     </Box>
