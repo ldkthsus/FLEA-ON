58aa11bebaf900ddc1d6cd7845572c6680e7b9ee
라송연 <skthddus123@naver.com>
2024-08-11 00:30:39 +0900

    fix : 수정

--- backend/src/main/java/com/ssafy/fleaOn/web/repository/CategoryRepository.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.fleaOn.web.repository;
 
 import com.ssafy.fleaOn.web.domain.Category;
+import com.ssafy.fleaOn.web.domain.Live;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -19,6 +20,8 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Category findByFirstCategoryNameAndSecondCategoryName(String firstCategoryName, String secondCategoryName);
 
     List<Category> findAllByFirstCategoryId(int firstCategoryId);
+
+    Optional<List<Category>> findAllByFirstCategoryNameOrSecondCategoryName(String firstCategoryName, String secondCategoryName);
 
     Optional<Category> findAllByFirstCategoryNameAndSecondCategoryName(String firstCategoryName, String secondCategoryName);
 
