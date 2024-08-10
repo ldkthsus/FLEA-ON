8fafd7945ee9687334a91687b2c21e855b9d4a2f
minji <minipeach0923@gmail.com>
2024-08-10 15:44:11 +0900

    feat : 사용자 fcm 등록

--- backend/src/main/java/com/ssafy/fleaOn/web/controller/UserApiController.java
+++ backend/src/main/java/com/ssafy/fleaOn/web/controller/UserApiController.java
@@ -75,6 +75,33 @@ public class UserApiController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("회원 탈퇴 실패: 토큰이 없습니다.");
     }
 
+    @Operation(summary = "fcm 등록", description = "사용자 디바이스를 등록할 때 사용합니다. ")
+    @PostMapping("/fcm")
+    public ResponseEntity<?> sendFcmToken(HttpServletRequest request, @RequestParam String fcmToken) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String jwtToken = authorizationHeader.substring(7).trim(); // "Bearer " 이후의 토큰만 추출하고 공백 제거
+            System.out.println("jwtToken: " + jwtToken);
+
+            try {
+                String email = JWTUtil.getEmail(jwtToken);
+                User user = userService.findByEmail(email);
+
+                if (user != null) {
+                    User savedUser = userService.updateFcmToken(user, fcmToken);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("fcm 등록 실패: 사용자를 찾을 수 없습니다.");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return new ResponseEntity<>("fcm 등록 중 오류가 발생하였습니다: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fcm 등록 실패: 토큰이 없습니다.");
+    }
+
     @Operation(summary = "회원 정보 조회", description = "사용자의 회원정보를 조회할 때 사용합니다. ")
     @GetMapping("/{email}/info")
     public ResponseEntity<?> getUserInfo(@PathVariable("email") String email) {
