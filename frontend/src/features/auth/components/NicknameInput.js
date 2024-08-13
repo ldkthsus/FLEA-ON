aab752a5db22c6289ca21b1c85e31098a15f4bfd
9oogone <attractiveness@naver.com>
2024-08-13 16:31:42 +0900

    design: 회원가입 로그인 initial

--- frontend/src/features/auth/components/NicknameInput.js
+++ frontend/src/features/auth/components/NicknameInput.js
@@ -37,6 +37,12 @@ const NicknameInput = ({ onNext }) => {
         color={isButtonEnabled ? "secondary" : "primary"}
         disabled={!isButtonEnabled}
         fullWidth
+        sx = {{
+          boxShadow: "none",
+          width: "100%",
+          height: "46px",
+          letterSpacing: "-0.5px"
+        }} 
       >
         계속하기
       </Button>
